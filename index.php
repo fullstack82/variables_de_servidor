168889e86cad464067a38e87e97822346ca1b2df
fullstack82 <fullstack8210@gmail.com>
2021-01-28 11:20:27 +0100

    variables del servidor

--- index.php
+++ index.php
@@ -1,6 +1,32 @@
 <?php
 
 
-// Variables super-globales de el servidor
+                        // Variables super-globales de el servidor
 
 // Variables de servidor
+
+echo '<h1>';
+echo $_SERVER['SERVER_ADDR'];  // Nos muestra la id del servidor
+echo '</h1>';
+
+echo '<h1>';
+echo $_SERVER['SERVER_NAME'];  // Nos muestra el nombre del servidor
+echo '</h1>';
+
+echo '<h1>';
+echo $_SERVER['SERVER_SOFTWARE'];  // Nos muestra el software que estamos usando
+echo '</h1>';
+
+echo '<h1>';
+echo $_SERVER['SERVER_PROTOCOL'];  // Nos muestra el protocolo que estamos usando
+echo '</h1>';
+
+echo '<h1>';
+echo $_SERVER['HTTP_USER_AGENT'];  // Nos muestra el navegador web  que estamos usando
+echo '</h1>';
+
+echo '<h1>';
+echo $_SERVER['REMOTE_ADDR'];  // Nos muestra la dirección del cliente  que estamos usando
+echo '</h1>';
+
+
